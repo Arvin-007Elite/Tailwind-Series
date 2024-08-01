50a9708c8f9fe4092247f7db58a81280aa905145
Arvin <arvinkritik2@gmail.com>
2024-08-01 12:52:55 +0530

    init

---
+++ TailwindCLIProject/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+   
+    extend: {
+      fontFamily: {
+        'kalnia-glaze': ['Kalnia Glaze', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
+
